676e3c00c571e3aeb1f8e8e939a4aa128e6b7a52
Emily Alava <emilyalava2004@gmail.com>
2025-05-15 15:32:01 -0500

    botones

---
+++ funciones.js
@@ -0,0 +1,12 @@
+function galeriajack() 
+{
+    window.location.href = "jack.html";
+}
+function galeriafernanda() 
+{
+    window.location.href = "fernanda.html";
+}
+function galeriadanilson() 
+{
+    window.location.href = "danilson.html";
+}
